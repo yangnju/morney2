63450e8375820cd3c7a09c20d2c1abe93880940d
CaramelStark <449134875@qq.com>
2021-04-29 12:28:53 +0800

    封装model

---
+++ src/model.ts
@@ -0,0 +1,16 @@
+// 为了避免每次用到数据的时候都要parse，stringify，所以根据MVC思想，将数据封装到model中
+// 因为不希望别人改变量名，所以声明一个 localStorageKeyName
+const localStorageKeyName = 'recordList';
+const model = {
+  clone(data: RecordItem[] | RecordItem) {
+    return JSON.parse(JSON.stringify(data));
+  },
+  fetch() {
+    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+  },
+  save(data: RecordItem[]) {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  }
+};
+
+export default model;
