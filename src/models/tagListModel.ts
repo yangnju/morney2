c044e981bf51a59e8f7ec4829a640cb76aaf045b
CaramelStark <449134875@qq.com>
2021-05-01 14:34:46 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -11,6 +11,8 @@ type TagListModel = {
   data: Tag[]
   fetch: () => Tag[]
   create: (name: string) => 'success' | 'duplicated' // 联合类型，不属于7中类型，类似于枚举（如果返回类型不是这两个字符串，就会报错，以免出现拼错导致永远无法报错。
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  remove: (id: string) => boolean
   save: () => void // 没有返回值
 }
 
@@ -31,6 +33,36 @@ const tagListModel: TagListModel = {
     this.save();
     return 'success';
   },
+  update(id, name) {
+      // 根据id找到对应的tag，如果找到了，就拿到对应tag的name，如果name已存在，就提示重复了，否则就替换掉，保存，返回成功
+      const idList = this.data.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          return 'duplicated';
+        } else {
+          const tag = this.data.filter(item => item.id === id)[0];
+          tag.name = name;
+          this.save();
+          return 'success';
+        }
+      } else {
+        return 'not found';
+      }
+    },
+
+    remove(id: string) {
+      let index = -1;
+      for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      this.data.splice(index, 1);
+      this.save();
+      return true;
+    },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,17 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="update"
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -26,6 +28,7 @@ import Button from '@/components/Button.vue';
 })
 export default class EditLabel extends Vue {
   tag?: { id: string, name: string } = undefined;
+
   created() {
     // 安装了vue-rooter，所以可以直接$route
     const id = this.$route.params.id;
@@ -39,6 +42,23 @@ export default class EditLabel extends Vue {
       // 为了让用户可以返回，所以用replace，而不是push
       this.$router.replace('/404');
     }
+  }
+
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+
+  remove() {
+    if (this.tag) {
+      tagListModel.remove(this.tag.id);
+    }
+  }
+
+  goBack() {
+    console.log('back');
+    this.$router.back();
   }
 }
 </script>
