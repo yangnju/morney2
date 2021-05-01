052d715d654a58c2b27af0cc6d3285aeed1e2ac3
CaramelStark <449134875@qq.com>
2021-05-01 12:00:19 +0800

    添加 EditLabel.vue 的样式，抽离了两个组件 Button 和 FormItem

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       <span class="name">{{this.fieldName}}</span>
       <input type="text"
              v-model="value"
@@ -14,7 +14,7 @@
   import {Component,Prop,Watch} from 'vue-property-decorator';
 
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = '';
 
     // 从外部获取左侧名称和空状态值，变成通用组件
@@ -29,7 +29,7 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
     background: #f5f5f5;
     padding-left: 16px;
@@ -39,7 +39,7 @@
       padding-right: 16px;
     }
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
