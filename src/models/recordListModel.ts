1d72d178edd19bf7fd42debeaef7bfe31dc54976
CaramelStark <449134875@qq.com>
2021-05-02 19:29:39 +0800

    将 create 封装到 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,11 +1,14 @@
+import clone from '@/lib/clone';
+
 // 为了避免每次用到数据的时候都要parse，stringify，所以根据MVC思想，将数据封装到model中
 // 因为不希望别人改变量名，所以声明一个 localStorageKeyName
 const localStorageKeyName = 'recordList';
 const recordListModel = {
   data: [] as RecordItem[],
-
-  clone(data: RecordItem[] | RecordItem) {
-    return JSON.parse(JSON.stringify(data));
+  create(record: RecordItem) {
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   },
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
