e06eeaeb7989d2a34c93463877b1e0657f6b5522
CaramelStark <449134875@qq.com>
2021-05-05 14:06:00 +0800

    使用 Vuex 的方法演示

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,8 +9,6 @@
       />
     </div>
     <Tags/>
-    {{count}}
-    <button @click="add">+1</button>
   </Layout>
 </template>
 
@@ -21,23 +19,18 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
+import oldStore from '@/store/index2';
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad},
   computed: {
-    count() {
-      return store.count;
-    },
     recordList() {
-      return store.recordList;
+      return this.$store.state.count
     }
   }
 })
 export default class Money extends Vue {
-  add() {
-    store.addCount();
-  }
+  recordList = oldStore.recordList
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -47,7 +40,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    store.createRecord(this.record);
+    oldStore.createRecord(this.record);
   }
 }
 </script>
