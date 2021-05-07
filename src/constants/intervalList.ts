ff0a566b669eee18da409486668bdea06b08904b
CaramelStark <449134875@qq.com>
2021-05-07 12:33:12 +0800

    创建常量文件，在 Money.vue 里面使用 Tabs

---
+++ src/constants/intervalList.ts
@@ -0,0 +1,6 @@
+// Object.freeze 保证不能被添加，不能被修改
+export default Object.freeze([
+  {text: '按天', value: 'day'},
+  {text: '按周', value: 'week'},
+  {text: '按月', value: 'month'},
+]);
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
@@ -15,13 +16,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad},
+  components: {Tags, FormItem, Tabs, NumberPad},
 })
 export default class Money extends Vue {
   record: RecordItem = {
@@ -30,6 +32,7 @@ export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
+  recordTypeList = recordTypeList;
   created(){
     this.$store.commit('fetchRecords')
   }
